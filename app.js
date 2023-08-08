09698319c4b84fcff3465cb511e877ebc5e2bb8b
makson <example@gmail.com>
2023-08-08 15:26:37 +0300

    adaptation V.2.1

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('burger-active');
     menuburger.classList.toggle('disp'); // Змініть тут клас на 'disp'
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('.carousel');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+    const images = document.querySelectorAll('.carousel img');
+
+    let currentIndex = 0;
+
+    prevBtn.addEventListener('click', function() {
+      currentIndex--;
+      if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+      }
+      updateCarousel();
+    });
+
+    nextBtn.addEventListener('click', function() {
+      currentIndex++;
+      if (currentIndex >= images.length) {
+        currentIndex = 0;
+      }
+      updateCarousel();
+    });
+
+    function updateCarousel() {
+      carousel.style.transform = `translateX(-${currentIndex * 100 / images.length}%)`;
+    }
+  });
